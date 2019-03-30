bc76b69220b39583f64d9d6a1eb7021c567fe923
joaoUltPc <zn_john@hotmail.com>
2019-03-30 17:17:02 -0300

    Fazendo funcionar o código para pegar apenas um pedaço do código

--- _py/aula-3/src/codigo1.py
+++ _py/aula-3/src/codigo1.py
@@ -1,3 +1,16 @@
+
+# inicio codigo 1
 class FormaGeometrica(): 
     def calc_area(self):
         pass
+
+# fim codigo 1
+
+
+# inicio codigo 2
+class classe2(): 
+    def calc_area(self):
+        pass
+
+# fim codigo 2
+
--- logica/logica1.js
+++ logica/logica1.js
@@ -9,7 +9,36 @@ $.ajax({url: "/figuras/2019-03-22-oi-joao.json"}).done(function( data ) {
 
     $.ajax({url: url_codigo}).done(function( data ) {
 
-      var textnode = document.createTextNode(data);         // Create a text node                            // Append the text to <li>
+      var re_compare = /\s*\n\s*/;
+      let token = data.split(re_compare)
+
+      // /\s*\n\s*/
+      var re_text = "\n";
+      let token_text = data.split(re_text)
+
+      let control = false
+      let texto = ""
+
+      token_text.forEach( element =>{
+
+          if(Number(element.replace("# inicio codigo ","")) === Number(1)){
+            control = true
+          }
+          if(Number(element.replace("# fim codigo ","")) === Number(1)){
+            control = false
+          }
+        
+        
+        
+        if(control){
+          texto = texto +"\n"+ element
+          console.log(texto)
+        }
+      })
+
+
+
+      var textnode = document.createTextNode(texto);         // Create a text node                            // Append the text to <li>
       document.getElementById(id_codigo).appendChild(textnode); 
 
       Prism.highlightElement($("#"+id_codigo)[0]);
